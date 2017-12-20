c6ec127e8948bb2c5624cc16f8fad5594863d408
Scott Sumner <scott.sumner@coreos.com>
2017-12-20 16:42:23 -0700

    for demo

--- app/views.py
+++ app/views.py
@@ -5,4 +5,4 @@ import datetime
 @app.route('/index')
 def index():
 	timestamp = str(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
-	return timestamp + " Hello, World! v3.0\n"
+	return timestamp + " Hello, World! v4.0\n"
